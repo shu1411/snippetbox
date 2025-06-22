1b9e7d5f3bfea4c3944a9c72322d75c63db5a6d7
shu <106082751+shu1411@users.noreply.github.com>
2025-06-22 14:32:36 +0200

    Add dependency injection through application struct

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,27 +7,38 @@ import (
 	"os"
 )
 
+type application struct {
+	logger *slog.Logger
+}
+
 func main() {
+	// set up command-line flags
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
-	// using nil for default settings
+	// set up logger, using nil for default settings
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
+	// set up app for handler dependencies
+	app := &application{
+		logger: logger,
+	}
+
+	// set up Server
 	mux := http.NewServeMux()
 
+	// file server and handlers
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-	mux.HandleFunc("GET /snippet/create", snippetCreate)
-	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
+	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
+	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
+	// logging
 	logger.Info("starting server", "addr", *addr)
-
-	err := http.ListenAndServe(*addr, mux)
-	logger.Error(err.Error())
+	logger.Error(http.ListenAndServe(*addr, mux).Error())
 
 	os.Exit(1)
 }
