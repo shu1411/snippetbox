9f0e9e8b6823d413ccafad26690c99f549b55009
shu <106082751+shu1411@users.noreply.github.com>
2025-08-11 17:38:47 +0200

    moved main.go and handlers.go into cmd/web

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,6 +7,7 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Server", "Go")
 	fmt.Fprintf(w, "Hello from Snippetbox")
 }
 
@@ -24,5 +25,6 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
 	fmt.Fprintf(w, "Save a new snippet...")
 }
