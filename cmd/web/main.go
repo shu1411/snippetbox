d76496869873e4601d827e5942e98614caa701c1
shu <106082751+shu1411@users.noreply.github.com>
2025-08-11 20:37:23 +0200

    move handlers into routes.go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -21,20 +21,7 @@ func main() {
 		logger: logger,
 	}
 
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
-	// restrict this route to exact matches on '/' only
-	mux.HandleFunc("GET /{$}", app.home)
-
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-
 	logger.Info("starting server", "addr", *addr)
-
-	logger.Error(http.ListenAndServe(*addr, mux).Error())
+	logger.Error(http.ListenAndServe(*addr, app.routes()).Error())
 	os.Exit(1)
 }
